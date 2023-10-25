59f52448bfebaaf7628422f9f823b52786261083
Tuomas Liinamaa <tlii@iki.fi>
2023-10-25 12:33:18 +0300

    Improved and fixed functions.

--- dotfiles.lib.sh
+++ dotfiles.lib.sh
@@ -14,31 +14,43 @@ check_repo() {
 check_variables() {
     [[ -z $DOTFILES_DIR ]] && DOTFILES_DIR="$HOME/.dotfiles";
     [[ -n $DOTFILES_DIR ]] && DOTFILES_DIR="${DOTFILES_DIR%/}";
-    [[ ! -d $DOTFILES_DIR ]] && (
+    [[ -z $DOTFILES_DIR ]] && DOTFILES_DIR="https://github.com/TLii/dotfiles.git"
+
+    # If not yet installed, install, and if installed, update.
+    if [[ ! -d $DOTFILES_DIR ]]; then
+
         [[ -z $DOTFILES_REPO_ADDRESS ]] && echo "Failed to get repository address" && exit 1;
-    );
+
+        mkdir -p "$DOTFILES_DIR";
+        git clone -q https://github.com/TLii/dotfiles.git "$DOTFILES_DIR" || echo "Failed to install dotfiles" && exit 1;
+
+    else
+
+        git pull -q https://github.com/TLii/dotfiles.git "$DOTFILES_DIR" || echo "Failed to update dotfiles";
+
+    fi
 
     # Export variables
     export DOTFILES_DIR="$DOTFILES_DIR"
     export DOTFILES_REPO_ADDRESS="$DOTFILES_REPO_ADDRESS"
+
 }
 
 # Link .*rc to corresponding dotfile rc's.
 install_rcfiles() {
-    for rc in $(ls ~/.dotfiles/*.rc); do
+    for rc in "$DOTFILES_DIR"/*.rc; do
         rclink=${rc%.rc}
         rclink=${rclink#$DOTFILES_DIR}
         rclink=${rclink#/}
         rclink="$HOME/.$rclink"
-        echo "I should link $rc to $rclink"
         if [[ -e $rclink ]]; then
             if [[ -L $rclink ]]; then
-                rm $rclink
+                rm "$rclink"
             else
-                mv $rclink $rclink.old
+                mv "$rclink" "$rclink.old"
             fi
         fi
-        ln -s $rc $rclink
+        ln -s "$rc" "$rclink"
     done
 }
 
